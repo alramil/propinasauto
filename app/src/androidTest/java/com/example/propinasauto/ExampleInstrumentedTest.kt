defd306f5c5510cb1fd054457452d2b4c074088f
alramil <alramil@unirioja.es>
2023-02-27 18:51:46 +0100

    ultcommit

---
+++ app/src/androidTest/java/com/example/propinasauto/ExampleInstrumentedTest.kt
@@ -0,0 +1,44 @@
+package com.example.propinasauto
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import com.example.propinasauto.ui.theme.PropinasautoTheme
+import org.junit.Rule
+import java.text.NumberFormat
+
+
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun calculate_20_percent_tip() {
+        composeTestRule.setContent {
+            PropinasautoTheme {
+                Surface (modifier = Modifier.fillMaxSize()){
+                    TipTimeScreen()
+                }
+            }
+        }
+        composeTestRule.onNodeWithText("Bill Amount").performTextInput("10")
+        composeTestRule.onNodeWithText("Tip (%)").performTextInput("20")
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        composeTestRule.onNodeWithText("Tip Amount: $expectedTip").assertExists(
+            "No node with this text was found."
+        )
+    }
+
+}
